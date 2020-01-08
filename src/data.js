44095b97318bfd3cc71ed73fec5c35288e2cec4c
bianca vicencio <bianquita@Air-de-monita.iptv.microsoft.com>
2020-01-08 01:41:03 -0300

    se agregan Filtros por genero y rated + descripciones

--- src/data.js
+++ src/data.js
@@ -1,14 +1,7 @@
-// fetch('http://www.omdbapi.com/?s&apikey=532b53fa')
-// .then(res => res.json())
-// .then(data => console.log(data))
-// .catch(error => console.log(error))
 
-//Cambiar de FIRST PAGE a SECOND PAGE al hacer click en la imagen 
-document.getElementById('allDisney').addEventListener('click', () => {
-    firstPage.style.display = 'none';
-    secondPage.style.display = 'block';
-  });
-
+//=======================================================================================================================//
+//=================================SECCION EN DONDE SE ARMAN LOS ARREGLOS CON LAS PELICULAS==============================//
+//=======================================================================================================================//
 const bestDisneyMovies = [
     {title:'Aladdin', imdbID:'tt0103639'},
     {title:'Beauty and the Beast', imdbID:'tt0101414'},
@@ -33,7 +26,6 @@ const bestDreamworksMovies =[
     {Title:'Bee Movie', imdbID:'tt0389790'},
     {Title:'Antz', imdbID:'tt0120587'}
 ];
-// console.log(bestDreamworksMovies);
 
 const bestGhibliMovies = [
     {Title:'My Neighbor Totoro', imdbID:'tt0096283'},
@@ -68,24 +60,314 @@ const bestPixarMovies = [
     {title:'The Good Dinosaur', imdbID:'tt1979388'},
 ];
 
-const movieBase = document.getElementById('showMovies');
 
-const allMovies = [].concat(bestDisneyMovies, bestDreamworksMovies, bestGhibliMovies, bestPixarMovies);
+//=======================================================================================================================//
+//===================================================DIVS PARA CADA ESTUDIO==============================================//
+//=======================================================================================================================//
 
-movieBase.innerHTML = ''; 
-finalArray = [];  
-for (let i = 0; i<allMovies.length; i++){    
-    fetch('http://www.omdbapi.com/?i='+allMovies[i].imdbID+'&apikey=532b53fa')
-    .then(res => res.json())
-    .then(data => {
-      finalArray.push(data)  
-      movieBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
-      <div class="card" style="width: 18rem;">
-      <a href="index.html"><img src="${data.Poster}" id="img2" class="img-fluid" alt="poster"></a>
-      <div class="cardStyle">
-      <h3 class="cardTitle">${data.Title}</h3>
-      </div>
-      </div>
-      </div>`
-    }) 
-} 
+const disneyBase = document.getElementById('showDisney');
+const dreamworksBase = document.getElementById('showDreamworks');
+const pixarBase = document.getElementById('showPixar');
+const ghibliBase = document.getElementById('showGhibli');
+
+
+//=======================================================================================================================//
+//===================================================SECCION DE PAGINA DISNEY==============================================//
+//=======================================================================================================================//
+document.getElementById('allDisney').addEventListener('click', () => {
+    firstPage.style.display = 'none';
+    secondPage.style.display = 'block';
+    thirdPage.style.display = 'none';
+  });
+
+// se detecta el click de boton de disney para mostrar la descripcion  
+document.getElementById('allDisney').addEventListener('click',showDescriptionStudioDisney);
+
+//detectando select para filtrar por genero 
+const selectDisney = document.getElementById('genreSearchDisney');
+selectDisney.addEventListener("change", function() { 
+    const genreDisney = document.getElementById("genreSearchDisney").value;
+    showDisneyMovies(genreDisney);
+});
+
+//detectando select para filtrar por rated  
+const selectRatedDisney = document.getElementById('ratedSearchDisney');
+selectRatedDisney.addEventListener("change", function() { 
+    const ratedDisney = document.getElementById("ratedSearchDisney").value;
+    showDisneyMoviesForRated(ratedDisney);
+});
+
+//funcion que muestra la descripcion disney
+function showDescriptionStudioDisney (){
+let studioDisney= document.getElementById('descriptionStudioDisney')
+studioDisney.style.display = 'block';
+}
+
+//funcion que filtra peliculas disney por genero
+function showDisneyMovies (genreDisney) { 
+    disneyBase.innerHTML = ''; //limpiando div
+    let finalData;  
+        for (let i = 0; i<bestDisneyMovies.length; i++) {    
+            fetch('http://www.omdbapi.com/?i='+bestDisneyMovies[i].imdbID+'&apikey=532b53fa')
+            .then(res => res.json())
+            .then(data => { //todos los elementos caen en data
+            finalData = (data.Genre.includes(genreDisney)?data:'');
+            if(finalData !== ''){
+                disneyBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card" style="width: 18rem;">
+                <a href="index.html"><img src="${finalData.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                <div class="cardStyle">
+                <h3 class="cardTitle">${finalData.Title}</h3>
+                </div>
+                </div>
+                </div>`
+            }    
+            })
+        }
+    }
+
+//funcion que filtra peliculas disney por rated
+    function showDisneyMoviesForRated (ratedDisney) { 
+        disneyBase.innerHTML = '';
+        let finalData;  
+            for (let i = 0; i<bestDisneyMovies.length; i++) {    
+                fetch('http://www.omdbapi.com/?i='+bestDisneyMovies[i].imdbID+'&apikey=532b53fa')
+                .then(res => res.json())
+                .then(data => {
+                finalData = (data.Rated.includes(ratedDisney)?data:'');
+                if(finalData !== ''){
+                    disneyBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card" style="width: 18rem;">
+                    <a href="index.html"><img src="${finalData.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                    <div class="cardStyle">
+                    <h3 class="cardTitle">${finalData.Title}</h3>
+                    </div>
+                    </div>
+                    </div>`
+                }    
+                })
+            }
+    }
+
+//=======================================================================================================================//
+//===================================================SEGION DE PAGINA DREAMWORKS==============================================//
+//=======================================================================================================================//
+document.getElementById('allDreamworks').addEventListener('click', () => {
+    firstPage.style.display = 'none';
+    secondPage.style.display = 'none';
+    thirdPage.style.display = 'block';
+  });
+document.getElementById('allDreamworks').addEventListener('click', showDescriptionStudioDreamwork);
+
+const selectDreamworks = document.getElementById('genreSearchDreamworks');// llamar al id del select
+selectDreamworks.addEventListener("change", function() { 
+    const genreDreamworks = document.getElementById("genreSearchDreamworks").value;
+    showDreamworksMovies(genreDreamworks);
+});
+
+//detectando select para filtrar por rated
+const selectRatedDreamwork = document.getElementById('ratedSearchDreamworks');// llamar al id del select
+selectRatedDreamwork.addEventListener("change", function() { 
+    const ratedDreamwork = document.getElementById("ratedSearchDreamworks").value;
+    showDreamworkMoviesForRated(ratedDreamwork);
+});
+
+function showDescriptionStudioDreamwork (genreDreamworks){
+    let studioDreamwork= document.getElementById('descriptionStudioDreamwork')
+    studioDreamwork.style.display = 'block';
+}
+
+function showDreamworksMovies (genreDreamworks) { 
+    dreamworksBase.innerHTML = '';
+    let finalData;  
+        for (let i = 0; i<bestDreamworksMovies.length; i++) {    
+            fetch('http://www.omdbapi.com/?i='+bestDreamworksMovies[i].imdbID+'&apikey=532b53fa')
+            .then(res => res.json())
+            .then(data => {
+            finalData = (data.Genre.includes(genreDreamworks)?data:'');
+            if(finalData !== ''){
+                dreamworksBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card" style="width: 18rem;">
+                <a href="index.html"><img src="${finalData.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                <div class="cardStyle">
+                <h3 class="cardTitle">${finalData.Title}</h3>
+                </div>
+                </div>
+                </div>`
+            }
+            })
+        }
+    }
+
+    //filtro peliculas dreamwork por rated
+    function showDreamworkMoviesForRated (ratedDreamwork) { 
+        dreamworksBase.innerHTML = '';
+        let finalData;  
+        for (let i = 0; i<bestDreamworksMovies.length; i++) {    
+            fetch('http://www.omdbapi.com/?i='+bestDreamworksMovies[i].imdbID+'&apikey=532b53fa')
+            .then(res => res.json())
+            .then(data => {
+            finalData = (data.Rated.includes(ratedDreamwork)?data:'');
+            if(finalData !== ''){
+                dreamworksBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card" style="width: 18rem;">
+                <a href="index.html"><img src="${finalData.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                <div class="cardStyle">
+                <h3 class="cardTitle">${finalData.Title}</h3>
+                </div>
+                </div>
+                </div>`
+            }
+            })
+        }
+        }
+
+//=======================================================================================================================//
+//===================================================SEGION DE PAGINA PIXAR==============================================//
+//=======================================================================================================================//
+document.getElementById('allPixar').addEventListener('click', () => {
+    firstPage.style.display = 'none';
+    secondPage.style.display = 'none';
+    thirdPage.style.display = 'none';
+    fourthPage.style.display = 'block';
+  });
+
+document.getElementById('allPixar').addEventListener('click', showDescriptionStudioPixar);
+
+const selectPixar = document.getElementById('genreSearchPixar');// llamar al id del select
+selectPixar.addEventListener("change", function() { 
+    const genrePixar = document.getElementById("genreSearchPixar").value;
+    showPixarMovies(genrePixar);
+});
+
+//detectando select para filtrar por rated
+const selectRatedPixar = document.getElementById('ratedSearchPixar');// llamar al id del select
+selectRatedPixar.addEventListener("change", function() { 
+    const ratedPixar = document.getElementById("ratedSearchPixar").value;
+    showPixarMoviesForRated(ratedPixar);
+});
+
+function showDescriptionStudioPixar (){
+    let studioPixar= document.getElementById('descriptionStudioPixar')
+    studioPixar.style.display = 'block';
+}
+
+function showPixarMovies (genrePixar) { 
+    pixarBase.innerHTML = '';
+    let finalData;  
+        for (let i = 0; i<bestPixarMovies.length; i++) {    
+            fetch('http://www.omdbapi.com/?i='+bestPixarMovies[i].imdbID+'&apikey=532b53fa')
+            .then(res => res.json())
+            .then(data => {
+            finalData = (data.Genre.includes(genrePixar)?data:'');
+                if(finalData !== ''){
+                    pixarBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class style="width: 18rem;">
+                    <a href="index.html"><img src="${finalData.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                    <div class="cardStyle">
+                    <h3 class="cardTitle">${finalData.Title}</h3>
+                    </div>
+                    </div>
+                    </div>`
+                }
+            })
+        }
+    }
+
+    function showPixarMoviesForRated(ratedPixar) { 
+        pixarBase.innerHTML = '';
+        let finalData;  
+            for (let i = 0; i<bestPixarMovies.length; i++) {    
+                fetch('http://www.omdbapi.com/?i='+bestPixarMovies[i].imdbID+'&apikey=532b53fa')
+                .then(res => res.json())
+                .then(data => {
+                finalData = (data.Rated.includes(ratedPixar)?data:'');
+                    if(finalData !== ''){
+                        pixarBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class style="width: 18rem;">
+                        <a href="index.html"><img src="${finalData.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                        <div class="cardStyle">
+                        <h3 class="cardTitle">${finalData.Title}</h3>
+                        </div>
+                        </div>
+                        </div>`
+                    }
+                })
+            }
+        }
+
+//=======================================================================================================================//
+//===================================================SEGION DE PAGINA GHIBLI==============================================//
+//=======================================================================================================================//
+document.getElementById('allGhibli').addEventListener('click', () => {
+    firstPage.style.display = 'none';
+    secondPage.style.display = 'none';
+    thirdPage.style.display = 'none';
+    fourthPage.style.display = 'none';
+    fifthPage.style.display = 'block';
+  });
+
+document.getElementById('allGhibli').addEventListener('click', showDescriptionStudioGhibli);
+
+const selectGhibli = document.getElementById('genreSearchGhibli');// llamar al id del select
+selectGhibli.addEventListener("change", function() { 
+    const genreGhibli = document.getElementById("genreSearchGhibli").value;
+    showGhibliMovies(genreGhibli);
+});
+
+//detectando select para filtrar por rated
+const selectRatedGhibli = document.getElementById('ratedSearchGhibli');// llamar al id del select
+selectRatedGhibli.addEventListener("change", function() { 
+    const ratedGhibli = document.getElementById("ratedSearchGhibli").value;
+    showGhibliMoviesForRated(ratedGhibli);
+});
+
+function showDescriptionStudioGhibli (){
+    let studioGhibli= document.getElementById('descriptionStudioGhibli')
+    studioGhibli.style.display = 'block';
+}
+
+function showGhibliMovies (genreGhibli) { 
+    ghibliBase.innerHTML = '';
+    let finalData;
+        for (let i = 0; i<bestGhibliMovies.length; i++) {    
+            fetch('http://www.omdbapi.com/?i='+bestGhibliMovies[i].imdbID+'&apikey=532b53fa')
+            .then(res => res.json())
+            .then(data => {
+            finalData = (data.Genre.includes(genreGhibli)?data:'');
+            if(finalData !== ''){
+                ghibliBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="card" style="width: 18rem;">
+                <a href="index.html"><img src="${data.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                <div class="cardStyle">
+                <h3 class="cardTitle">${data.Title}</h3>
+                </div>
+                </div>
+                </div>`
+            }
+            })
+        }
+    }
+
+    function showGhibliMoviesForRated(ratedGhibli) { 
+        ghibliBase.innerHTML = '';
+        let finalData;
+            for (let i = 0; i<bestGhibliMovies.length; i++) {    
+                fetch('http://www.omdbapi.com/?i='+bestGhibliMovies[i].imdbID+'&apikey=532b53fa')
+                .then(res => res.json())
+                .then(data => {
+                finalData = (data.Rated.includes(ratedGhibli)?data:'');
+                if(finalData !== ''){
+                    ghibliBase.innerHTML +=`<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card" style="width: 18rem;">
+                    <a href="index.html"><img src="${data.Poster}" id="img2" class="img-fluid" alt="poster"></a>
+                    <div class="cardStyle">
+                    <h3 class="cardTitle">${data.Title}</h3>
+                    </div>
+                    </div>
+                    </div>`
+                }
+                })
+            }
+        }
+
